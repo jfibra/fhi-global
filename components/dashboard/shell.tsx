9b2f19c62ee7475a01e7903e1e42512de53833ce
Wizzax <marklawrince730@gmail.com>
2026-07-23 23:25:17 +0800

    asdasd

--- components/dashboard/shell.tsx
+++ components/dashboard/shell.tsx
@@ -403,7 +403,7 @@ export function DashboardShell({
         </header>
 
         {/* Page content */}
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="flex-1 overflow-y-auto scrollbar-none p-6">
           {children}
         </main>
       </div>
